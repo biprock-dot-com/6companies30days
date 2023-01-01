a71872c762c0ae577085cf9e73b20376d946a16b
biprock-dot-com <biprajitsuklabaidya345@gmail.com>
2023-01-01 21:01:03 +0530

    3rd question solved

--- Microsoft/Bulls_and_cows.cpp
+++ Microsoft/Bulls_and_cows.cpp
@@ -1,8 +1,28 @@
-class solution
-{
-    public:
-     string gethint(string secret, string guess)
-     {
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char,int>m;
+        int bulls =0, cows =0;
 
-     }
+        for(int i=0; i<secret.size(); i++)
+        {
+           if(secret[i]==guess[i])
+             bulls++;
+           else 
+             m[secret[i]]++;
+
+        }
+
+        for(int i=0; i<secret.size(); i++)
+        {
+            if(secret[i]==guess[i] ) continue;
+            else if(m[guess[i]])
+            {
+                m[guess[i]]--;
+                cows++;
+            }
+        }
+        return (to_string(bulls) + "A" + to_string(cows) + "B");
+         
+    }
 };
