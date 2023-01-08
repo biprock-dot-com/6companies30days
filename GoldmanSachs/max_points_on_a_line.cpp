8247003d4c5a105ba4f1121f1f0c98c8df1c8709
Biprajit Suklabaidya <121727740+biprock-dot-com@users.noreply.github.com>
2023-01-08 14:46:32 +0530

    1st question solved

--- GoldmanSachs/max_points_on_a_line.cpp
+++ GoldmanSachs/max_points_on_a_line.cpp
@@ -1,1 +1,36 @@
-class solution
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+     sort(points.begin(), points.end());
+
+    int n = points.size(), count = 0;
+        
+    for(int i=0; i<n; ++i)
+    {
+        unordered_map<double, int> mp;
+        int y = 0;
+            
+        for(int j=i+1; j<n; ++j)
+        {
+            int y_ = points[j][1] - points[i][1];
+            int x_ = points[j][0] - points[i][0];
+                
+            if(x_==0)
+            {
+                y++;
+            }
+            else
+            {
+                double slope = (double)y_/x_;
+                mp[slope]++;
+            }
+            }
+            for(auto& m : mp)
+            {
+                count = max(count, m.second);
+            }
+            count = max(count, y);
+        }
+        return count+1;
+    }
+};
